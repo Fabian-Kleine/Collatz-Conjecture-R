c84987e537d4d255d8d0f2198f2dcf5818218278
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2023-11-14 16:13:34 +0100

    changed xlab

--- graph.R
+++ graph.R
@@ -1,6 +1,6 @@
 render_graph <- function(nums) {
   x_axis <- seq(from = 1, to = length(nums), by = 1)
   y_axis <- nums
-  plot(x_axis, y_axis, xlab = "Steps until 0", ylab = "Numbers")
+  plot(x_axis, y_axis, xlab = "Steps until 1", ylab = "Numbers")
   lines(x_axis, y_axis, col = "blue")
 }
