b6bc1601a58e6b4bde3766c108ce192eba539192
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2023-11-14 16:10:19 +0100

    changed color of graph

--- graph.R
+++ graph.R
@@ -2,5 +2,5 @@ render_graph <- function(nums) {
   x_axis <- seq(from = 1, to = length(nums), by = 1)
   y_axis <- nums
   plot(x_axis, y_axis, xlab = "Steps until 0", ylab = "Numbers")
-  lines(x_axis, y_axis, col = "gray")
+  lines(x_axis, y_axis, col = "blue")
 }
